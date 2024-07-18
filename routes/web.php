6df92df9ed9316f71fcd6cc55270c536bf5be43a
waseem akhter <waseemakhter028@gmail.com>
2024-07-18 21:51:52 +0530

    admin routes added

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Models\Category;
-use App\Models\SubCategory;
-use App\Models\Code;
+use App\Http\Controllers\SuperAdminController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CodeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,29 +17,35 @@ use App\Models\Code;
 |
 */
 
-Route::get('/', function () {
-        $allcat  = Category::where('status',1)->select('id')->get();
-        $category_ids  = [];
-        foreach($allcat  as $cat):
-            $subcat = SubCategory::where('category_id',$cat->id)->where('status',1)->first();
-            if(!empty($subcat))
-            {
-                $codecheck = Code::where('sub_category_id',$subcat->id)->where('status',1)->first();
-                if(!empty($codecheck))
-                {
-                    array_push($category_ids,$cat->id);
-                }
-
-            }
-        endforeach;
-
-        $category  = Category::whereIn('id',$category_ids)->where('status',1)->select('id','name')->get()->sortBy('name');
-
-        $codes = Code::where('status',1)->orderBy('id','DESC')->get();
-
-        return view('user.code')->withcategory($category)->withcodes($codes);
-});
-
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 Route::get('viewcode/{id}', [HomeController::class, 'viewCode']);
 Route::post('filtercodes', [HomeController::class, 'filtercodes']);
+
+########admin panel routes##################
+Route::group(['prefix' => 'adminpanel'], function () {
+    
+  Route::any('login', [SuperAdminController::class, 'login']);
+  Route::get('/', [SuperAdminController::class, 'index']);
+
+  Route::group(['middleware' => ['AdminAuth']], function() {
+
+    Route::any('logout', [SuperAdminController::class, 'logout']);
+
+    Route::get('dashboard', [SuperAdminController::class, 'dashboard']);
+    Route::post('changestatus', [SuperAdminController::class, 'changeStatus']);
+        ######category controller routes#########
+        Route::get('category/list', [CategoryController::class, 'category']);
+        Route::any('category/add', [CategoryController::class, 'categoryAdd']);
+        Route::any('category/edit/{id?}', [CategoryController::class, 'categoryEdit']);
+
+        Route::get('subcategory/list', [CategoryController::class, 'subcategory']);
+        Route::any('subcategory/add', [CategoryController::class, 'subcategoryAdd']);
+        Route::any('subcategory/edit/{id?}',  [CategoryController::class, 'subcategoryEdit']);
+
+        Route::get('code/list', [CodeController::class, 'code']);
+        Route::any('code/add', [CodeController::class, 'codeAdd']);
+        Route::any('code/edit/{id?}', [CodeController::class, 'codeEdit']);
+        Route::get('code/delete/{id?}', [CodeController::class, 'codeDelete']);
+        Route::post('code/subcategory', [CodeController::class, 'subcategory']);
+  });//for auth routes in admin panel
+});
