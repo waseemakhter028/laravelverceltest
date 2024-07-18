b4636fc60e0b4908efeecfa500f83c7a595d8e2a
waseem akhter <waseemakhter028@gmail.com>
2024-07-18 20:28:54 +0530

    view added

---
+++ resources/views/user/include/header.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+
+<!-- Mirrored from preview.colorlib.com/theme/malefashion/shop.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 28 Oct 2020 14:52:16 GMT -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Male_Fashion Template">
+    <meta name="keywords" content="Male_Fashion, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WS Blog | @yield('title')</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&amp;display=swap"
+    rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="{{asset('/web/')}}/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('/web/')}}/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('/web/')}}/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('/web/')}}/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('/web/')}}/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('/web/')}}/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('/web/')}}/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('/web/')}}/css/style.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('/web/')}}/css/prism.css" type="text/css">
+    <link rel="icon" href="{{asset('/admin_assets/assets/images/fav.png')}}" type="image/x-icon">
+@yield('csscode')
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+   
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="header__top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-7">
+                        <div class="header__top__left">
+                            <p>WS Blog Code  List</p>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3">
+                    <div class="header__logo">
+                        <a href="index.html"><img src="img/logo.png" alt=""></a>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="canvas__open"><i class="fa fa-bars"></i></div>
+        </div>
+    </header>
+    <!-- Header Section End -->
