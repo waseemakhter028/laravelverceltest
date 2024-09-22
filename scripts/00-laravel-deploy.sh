301630365f3fe4735cec80630332c577f790ebc4
waseem akhter <waseemakhter028@gmail.com>
2024-09-23 03:17:33 +0530

    composer dump

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,8 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+composer dump-autoload --working-dir=/var/www/html
+
 echo "generating application key..."
 php artisan key:generate --show
 
